4ed1571973b1a27387f95d71cbd1482612c70d82
DYung26 <theadstore1407@gmail.com>
2024-03-23 04:33:46 -0700

    Double linked list

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -44,8 +44,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else
 	{
 		(*head)->prev->next = (*head)->next;
-		(*head)->next->prev = (*head)->prev;
 		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
 		/*
 		* ALTERNATIVE
 		* if ((*head)->prev)
